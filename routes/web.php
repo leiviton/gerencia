3a3c1875c5a1a0f478c3b6519858002415153e73
Leiviton Carlos <leivitoncs@gmail.com>
2017-11-28 21:21:06 -0200

    seed

--- routes/web.php
+++ routes/web.php
@@ -17,28 +17,29 @@ Route::get('/', function () {
 });
 
 
+Route::group(['middleware'=>'cors'],function (){
+    Route::group(['prefix'=>'api/v1'],function () {
 
-Route::group(['prefix'=>'api/v1'],function () {
+        Route::group(['prefix'=>'admin','namespace'=>'Api\V1\Deliveryman','middleware' => 'auth:api'], function (){
+            Route::get('orders','DeliverymanCheckoutController@index');
+            Route::get('filters','DeliverymanCheckoutController@getOrderFiltros');
+            Route::get('auth/user','DeliverymanCheckoutController@authenticated');
+            Route::get('order/{id}','DeliverymanCheckoutController@show');
+        });
 
-    Route::group(['prefix'=>'admin','namespace'=>'Api\V1\Deliveryman','middleware' => 'auth:api'], function (){
-        Route::get('orders','DeliverymanCheckoutController@index');
-        Route::get('filters','DeliverymanCheckoutController@getOrderFiltros');
-        Route::get('auth/user','DeliverymanCheckoutController@authenticated');
-        Route::get('order/{id}','DeliverymanCheckoutController@show');
+        Route::group(['prefix' => 'admin','namespace'=>'Api\V1\Admin','middleware' => 'auth:api'],function (){
+           Route::post('order','AdminCheckoutController@store');
+           Route::get('groups','CategoriesController@index');
+           Route::get('subgroups','SubgroupsController@index');
+           Route::get('products', 'ProductsController@index');
+           Route::get('product/{id}', 'ProductsController@edit');
+           Route::post('product/store', 'ProductsController@store');
+           Route::put('product/{id}', 'ProductsController@update');
+           Route::get('search','AdminCheckoutController@search');
+           Route::get('mesas', 'AdminCheckoutController@getMesas');
+        });
+        Route::get('user','Api\UserController@authenticated');
     });
-
-    Route::group(['prefix' => 'admin','namespace'=>'Api\V1\Admin','middleware' => 'auth:api'],function (){
-       Route::post('order','AdminCheckoutController@store');
-       Route::get('groups','CategoriesController@index');
-       Route::get('subgroups','SubgroupsController@index');
-       Route::get('products', 'ProductsController@index');
-       Route::get('product/{id}', 'ProductsController@edit');
-       Route::post('product/store', 'ProductsController@store');
-       Route::put('product/{id}', 'ProductsController@update');
-       Route::get('search','AdminCheckoutController@search');
-       Route::get('mesas', 'AdminCheckoutController@getMesas');
-    });
-    Route::get('user','Api\UserController@authenticated');
 });
 
 Route::get('/home', 'HomeController@index')->name('home');
