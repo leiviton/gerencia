bf59f21fca1936d8c983bc50453b5af4ecb2525e
Leiviton Carlos <leivitoncs@gmail.com>
2017-11-29 00:13:05 -0200

    seed

--- config/app.php
+++ config/app.php
@@ -65,7 +65,7 @@ return [
     |
     */
 
-    'timezone' => env('APP_TIMEZONE'),
+    'timezone' => 'America/Sao_Paulo',
 
     /*
     |--------------------------------------------------------------------------
