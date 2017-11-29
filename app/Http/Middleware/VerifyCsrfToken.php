1d239daf6dd4a2df11c03ec7a053b881ae11e2ca
Leiviton Carlos <leivitoncs@gmail.com>
2017-11-28 23:19:54 -0200

    seed

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -14,6 +14,7 @@ class VerifyCsrfToken extends Middleware
     protected $except = [
         //
         'oauth/token',
-        'api/*'
+        'api/*',
+        'api/v1/admin/auth/user'
     ];
 }
