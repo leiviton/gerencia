5bc2ef42d25b10721cdf02fb35b369dc58d6d2c0
Leiviton Carlos <leivitoncs@gmail.com>
2017-12-29 18:59:43 -0200

    caixas

--- app/Services/ClientService.php
+++ app/Services/ClientService.php
@@ -46,13 +46,25 @@ class ClientService
     }
 
     public function create($data){
-        $user['name'] = $data['name'];
-        $user['email'] = $data['email'];
-        $user['password'] = bcrypt(123456);
-        $user = $this->userRepository->create($user);
-        $data['user_id'] = $user->id;
-        $address = $this->addressClientRepository->create($data['address']);
-        $data['address_client_id'] = $address->id;
-        return $this->clientRepository->create($data);
+        \DB::beginTransaction();
+        try {
+            $user['name'] = $data['name'];
+            $user['email'] = $data['email'];
+            $user['password'] = bcrypt(123456);
+            $user = $this->userRepository->create($user);
+            $address = $this->addressClientRepository->create($data['address']);
+            $client['user_id'] = $user->id;
+            $client['name'] = $data['name'];
+            $client['phone'] = $data['phone'];
+            $client['address'] = $address->address.', '.$address->numero.', '.$address->bairro.' - '.$address->city;
+            $client['status'] = 0;
+            $client['address_client_id'] = $address->id;
+            $result = $this->clientRepository->create($client);
+            \DB::commit();
+            return $result;
+        } catch (\Exception $e){
+            \DB::rollback();
+            throw $e;
+        }
     }
 }
