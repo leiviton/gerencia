60eee9de30754b879da577812f6007d398016ece
Leiviton <leivitonpj@gmail.com>
2018-12-19 18:50:06 -0200

    cors

--- config/cors.php
+++ config/cors.php
@@ -14,7 +14,7 @@ return [
     'supportsCredentials' => false,
     'allowedOrigins' => ['*'],
     'allowedHeaders' => ['*'],
-    'allowedMethods' => ['*'],
+    'allowedMethods' => ['GET', 'POST', 'PUT',  'DELETE','PATCH','OPTIONS'],
     'exposedHeaders' => [],
     'maxAge' => 0,
     'hosts' => [],
--- web/src/environments/environment.ts
+++ web/src/environments/environment.ts
@@ -5,7 +5,7 @@
 
 export const environment = {
     production: true,
-    server_url: 'http://108.61.155.169',
+    server_url: 'https://delivery.projectsoftware.com.br',
     client_id: '2',
     client_secret: 'fb7duCeekdxfWJI4gsmzoxV1vnVlmgVwIGlGgK2I'
 };
